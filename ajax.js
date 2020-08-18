6e233aecb5620780840b103cca2f8af8816b6957
Mariana Reis <marianaluizamr@gmail.com>
2020-08-18 18:33:39 +0100

    reorganising file names

---
+++ ajax.js
@@ -0,0 +1,13 @@
+function carregarDados() {
+    let xhr = XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if( xhr.readySate == 4 && xhr.status == 200){
+            document.getElementById('')
+        }
+
+
+
+    }
+
+}
